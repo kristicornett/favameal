7e6da3a5ac956bd4def9c54e3060706a77f6bdc6
Kristi Cornett <kristi@cornettclan.com>
2023-05-19 16:38:48 -0500

    model updates

--- favamealapi/models/meal.py
+++ favamealapi/models/meal.py
@@ -8,6 +8,31 @@ class Meal(models.Model):
     restaurant = models.ForeignKey("Restaurant", on_delete=models.CASCADE)
     # TODO: Establish a many-to-many relationship with User through the appropriate join model
 
+    @property
+    def joined(self):
+        return self.__joined
+    
+    @joined.setter
+    def joined(self, value):
+        self.__joined = value
+
     # TODO: Add an user_rating custom properties
 
+    @property
+    def user_rating(self):
+        return self.__user_rating
+    
+    @user_rating.setter
+    def user_rating(self, value):
+        self.__user_rating = value
+        
+
     # TODO: Add an avg_rating custom properties
+
+    @property
+    def avg_rating(self):
+        return self.__avg_rating
+    
+    @avg_rating.setter
+    def avg_rating(self, value):
+        self.__avg_rating = value 
